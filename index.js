b12ba02bd3fbdbf92c63e7c5f02054c80a18e1a3
NLJZ <johnson.nathan.lee@gmail.com>
2020-04-22 13:57:50 +0200

    Updates

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const colorGenerator = () => {
+  let randomColor = (Math.random().toString(16) + "0000000").slice(2, 8);
+  return randomColor;
+};
+
 const addToList = () => {
   let userInput = document.querySelector("#userText").value;
   console.log(userInput);
   let newLi = document.createElement("li");
   let text = document.createTextNode(userInput);
-  newLi.appendChild(text);
-  document.querySelector(".result").appendChild(newLi);
-  document.querySelector("#userText").value = "";
+  if (userInput !== "") {
+    let bgColor = `#${colorGenerator()}`;
+    newLi.appendChild(text);
+    newLi.style.backgroundColor = `${bgColor}`;
+    document.querySelector(".result").appendChild(newLi);
+    document.querySelector("#userText").value = "";
+  }
 };
