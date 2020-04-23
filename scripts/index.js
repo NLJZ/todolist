c05f3864fc4c57ef1fa088ee3853ea181bb0eddd
NLJZ <johnson.nathan.lee@gmail.com>
2020-04-23 11:33:36 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,19 @@ const addToList = () => {
     newLi.appendChild(text);
     document.querySelector(".result").appendChild(newLi);
     document.querySelector("#userText").value = "";
+    let span = document.createElement("button");
+    let closeText = document.createTextNode("x");
+    span.className = "close";
+    span.appendChild(closeText);
+
+    let listAll = document.querySelectorAll("li");
+    listAll.forEach((item) => {
+      item.appendChild(span);
+    });
+    let closeButtons = document.querySelectorAll(".close");
+    closeButtons.forEach((button) => {
+      button.addEventListener("click", closeIt, false);
+    });
   }
   let totalItems = document.querySelectorAll("li");
   let length = 0;
@@ -22,19 +35,6 @@ const addToList = () => {
     let bgColor = `#${colorGenerator()}`;
     newLi.style.backgroundColor = `${bgColor}`;
   }
-  let span = document.createElement("button");
-  let closeText = document.createTextNode("x");
-  span.className = "close";
-  span.appendChild(closeText);
-
-  let listAll = document.querySelectorAll("li");
-  listAll.forEach((item) => {
-    item.appendChild(span);
-  });
-  let closeButtons = document.querySelectorAll(".close");
-  closeButtons.forEach((button) => {
-    button.addEventListener("click", closeIt, false);
-  });
 };
 
 const closeIt = () => {
