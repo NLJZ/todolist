8aa81c69a56aa891f7ad584ac7a285c8ed24fcd0
NLJZ <johnson.nathan.lee@gmail.com>
2020-04-23 12:17:10 +0200

    added function to remove list item completely and adjust list colors

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,35 +8,47 @@ const addToList = () => {
   console.log(userInput);
   let newLi = document.createElement("li");
   let text = document.createTextNode(userInput);
-  if (userInput !== "") {
-    newLi.appendChild(text);
-    document.querySelector(".result").appendChild(newLi);
-    document.querySelector("#userText").value = "";
-    let span = document.createElement("button");
-    let closeText = document.createTextNode("x");
-    span.className = "close";
-    span.appendChild(closeText);
+  if (userInput == "") {
+    return alert("please write something");
+  }
+  newLi.appendChild(text);
+  document.querySelector(".result").appendChild(newLi);
+  document.querySelector("#userText").value = "";
+  let span = document.createElement("button");
+  let closeText = document.createTextNode("x");
+  span.className = "close";
+  span.appendChild(closeText);
 
-    let listAll = document.querySelectorAll("li");
-    listAll.forEach((item) => {
-      item.appendChild(span);
-    });
-    let closeButtons = document.querySelectorAll(".close");
-    closeButtons.forEach((button) => {
-      button.addEventListener("click", closeIt, false);
-    });
-  }
-  let totalItems = document.querySelectorAll("li");
-  let length = 0;
-  totalItems.forEach((element) => {
-    length++;
+  let listAll = document.querySelectorAll("li");
+  listAll.forEach((item) => {
+    item.appendChild(span);
   });
-  if (length % 2 !== 0) {
+  let closeButtons = document.querySelectorAll(".close");
+  closeButtons.forEach((button) => {
+    button.addEventListener("click", closeIt, false);
+  });
+  let oddItems = document.querySelectorAll("li:nth-child(odd)");
+  oddItems.forEach((element) => {
     let bgColor = `#${colorGenerator()}`;
-    newLi.style.backgroundColor = `${bgColor}`;
-  }
+    element.style.backgroundColor = `${bgColor}`;
+  });
+};
+
+const reColor = () => {
+  let oddItems = document.querySelectorAll("li:nth-child(odd)");
+  let evenItems = document.querySelectorAll("li:nth-child(even)");
+  oddItems.forEach((element) => {
+    let bgColor = `#${colorGenerator()}`;
+    element.style.backgroundColor = `${bgColor}`;
+  });
+
+  evenItems.forEach((element) => {
+    let bgColor = "#fff";
+    element.style.backgroundColor = `${bgColor}`;
+  });
 };
 
 const closeIt = () => {
-  document.activeElement.parentNode.style.display = "none";
+  document.activeElement.parentNode.remove();
+  reColor();
 };
